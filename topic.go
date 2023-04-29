9cf09a2057aecb4844e384c94d9f0a125b253a74
Daniil Suvorov <severecloud@gmail.com>
2023-04-29 14:13:01 +0300

    feat: add TopicsHot - closed #45

--- topic.go
+++ topic.go
@@ -5,13 +5,33 @@ import (
 	"time"
 )
 
+type Topic struct {
+	ID                int         `json:"id"`
+	TopicTitle        string      `json:"topic_title"`
+	Body              string      `json:"body"`
+	HTMLBody          string      `json:"html_body"`
+	HTMLFooter        string      `json:"html_footer"`
+	CreatedAt         time.Time   `json:"created_at"`
+	CommentsCount     int         `json:"comments_count"`
+	Forum             Forum       `json:"forum"`
+	User              User        `json:"user"`
+	Type              string      `json:"type"`
+	LinkedID          int         `json:"linked_id"`
+	LinkedType        string      `json:"linked_type"`
+	Linked            interface{} `json:"linked"`
+	Viewed            bool        `json:"viewed"`
+	LastCommentViewed bool        `json:"last_comment_viewed"`
+	Event             interface{} `json:"event"`
+	Episode           interface{} `json:"episode"`
+}
+
 type TopicsUpdate struct {
-	ID        int       `json:"id"`
-	Linked    Anime     `json:"linked"`
-	Event     string    `json:"event"`
-	Episode   int       `json:"episode"`
-	CreatedAt time.Time `json:"created_at"`
-	URL       string    `json:"url"`
+	ID        int         `json:"id"`
+	Linked    interface{} `json:"linked"`
+	Event     string      `json:"event"`
+	Episode   int         `json:"episode"`
+	CreatedAt time.Time   `json:"created_at"`
+	URL       string      `json:"url"`
 }
 
 type TopicsUpdatesParams struct {
@@ -22,8 +42,19 @@ type TopicsUpdatesParams struct {
 	Limit int `json:"limit,omitempty"`
 }
 
-func (s *API) TopicsUpdates(ctx context.Context, params *BansParams) (resp []TopicsUpdate, err error) {
+func (s *API) TopicsUpdates(ctx context.Context, params *TopicsUpdatesParams) (resp []TopicsUpdate, err error) {
 	err = s.get(ctx, &resp, "topics/updates", params)
+
+	return
+}
+
+type TopicsHotParams struct {
+	// 10 maximum.
+	Limit int `json:"limit,omitempty"`
+}
+
+func (s *API) TopicsHot(ctx context.Context, params *TopicsHotParams) (resp []Topic, err error) {
+	err = s.get(ctx, &resp, "topics/hot", params)
 
 	return
 }
--- topic_test.go
+++ topic_test.go
@@ -3,12 +3,25 @@ package shikimori_test
 import (
 	"context"
 	"testing"
+
+	"github.com/SevereCloud/shikimori"
 )
 
-func TestTopicsUpdate(t *testing.T) {
+func TestTopicsUpdates(t *testing.T) {
 	t.Parallel()
 
 	resp, err := shiki.TopicsUpdates(context.Background(), nil)
+
+	NoError(t, err)
+	NotEmpty(t, resp)
+}
+
+func TestTopicsHot(t *testing.T) {
+	t.Parallel()
+
+	resp, err := shiki.TopicsHot(context.Background(), &shikimori.TopicsHotParams{
+		Limit: 10,
+	})
 
 	NoError(t, err)
 	NotEmpty(t, resp)
