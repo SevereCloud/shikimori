60e16930149fc02f93c3bae17507d7478120a721
Daniil Suvorov <severecloud@gmail.com>
2023-05-07 15:39:22 +0300

    feat: add ranobe
    - closed #98
    - closed #99
    - closed #100
    - closed #101
    - closed #103
    - closed #104

---
+++ ranobe.go
@@ -0,0 +1,118 @@
+package shikimori
+
+import (
+	"context"
+	"strconv"
+)
+
+type Ranobe struct {
+	ID         int    `json:"id"`
+	Name       string `json:"name"`
+	Russian    string `json:"russian"`
+	Image      Image  `json:"image"`
+	URL        string `json:"url"`
+	Kind       string `json:"kind"`
+	Score      string `json:"score"`
+	Status     string `json:"status"`
+	Volumes    int    `json:"volumes"`
+	Chapters   int    `json:"chapters"`
+	AiredOn    string `json:"aired_on"`
+	ReleasedOn string `json:"released_on"`
+}
+
+type RanobeFull struct {
+	Ranobe
+
+	English            []string            `json:"english"`
+	Japanese           []string            `json:"japanese"`
+	Synonyms           []string            `json:"synonyms"`
+	LicenseNameRu      *string             `json:"license_name_ru"`
+	Description        string              `json:"description"`
+	DescriptionHTML    string              `json:"description_html"`
+	DescriptionSource  *string             `json:"description_source"`
+	Franchise          *string             `json:"franchise"`
+	Favoured           bool                `json:"favoured"`
+	Anons              bool                `json:"anons"`
+	Ongoing            bool                `json:"ongoing"`
+	ThreadID           *int                `json:"thread_id"`
+	TopicID            *int                `json:"topic_id"`
+	MyanimelistID      int                 `json:"myanimelist_id"`
+	RatesScoresStats   []RatesScoresStat   `json:"rates_scores_stats"`
+	RatesStatusesStats []RatesStatusesStat `json:"rates_statuses_stats"`
+	Licensors          []string            `json:"licensors"`
+	Genres             []Genre             `json:"genres"`
+	Publishers         []Publisher         `json:"publishers"`
+	UserRate           *UserRate           `json:"user_rate"`
+}
+
+type RanobeParams struct{}
+
+func (s *API) Ranobe(ctx context.Context, id int, params *RanobeParams) (resp RanobeFull, err error) {
+	err = s.get(ctx, &resp, "ranobe/"+strconv.Itoa(id), params)
+
+	return
+}
+
+type RanobeRole struct {
+	Roles        []string       `json:"roles"`
+	RolesRussian []string       `json:"roles_russian"`
+	Character    *CharacterBase `json:"character"`
+	Person       *PersonBase    `json:"person"`
+}
+
+type RanobeRolesParams struct{}
+
+func (s *API) RanobeRoles(
+	ctx context.Context, id int, params *RanobeRolesParams,
+) (resp []RanobeRole, err error) {
+	err = s.get(ctx, &resp, "ranobe/"+strconv.Itoa(id)+"/roles", params)
+
+	return
+}
+
+type RanobeSimilarParams struct{}
+
+func (s *API) RanobeSimilar(ctx context.Context, id int, params *RanobeSimilarParams) (resp []Ranobe, err error) {
+	err = s.get(ctx, &resp, "ranobe/"+strconv.Itoa(id)+"/similar", params)
+
+	return
+}
+
+type RanobeRelated struct {
+	Relation        string      `json:"relation"`
+	RelationRussian string      `json:"relation_russian"`
+	Anime           Anime       `json:"anime"`
+	Manga           interface{} `json:"manga"`
+}
+
+type RanobeRelatedParams struct{}
+
+func (s *API) RanobeRelated(ctx context.Context, id int, params *RanobeRelatedParams) (
+	resp []RanobeRelated,
+	err error,
+) {
+	err = s.get(ctx, &resp, "ranobe/"+strconv.Itoa(id)+"/related", params)
+
+	return
+}
+
+type RanobeExternalLinksParams struct{}
+
+func (s *API) RanobeExternalLinks(ctx context.Context, id int, params *RanobeExternalLinksParams) (
+	resp []ExternalLink,
+	err error,
+) {
+	err = s.get(ctx, &resp, "ranobe/"+strconv.Itoa(id)+"/external_links", params)
+
+	return
+}
+
+type RanobeTopicsParams struct{}
+
+func (s *API) RanobeTopics(
+	ctx context.Context, id int, params *RanobeTopicsParams,
+) (resp []Topic, err error) {
+	err = s.get(ctx, &resp, "ranobe/"+strconv.Itoa(id)+"/topics", params)
+
+	return
+}
---
+++ ranobe_test.go
@@ -0,0 +1,60 @@
+package shikimori_test
+
+import (
+	"context"
+	"testing"
+)
+
+func TestRanobe(t *testing.T) {
+	t.Parallel()
+
+	resp, err := shiki.Ranobe(context.Background(), 14893, nil)
+
+	NoError(t, err)
+	NotEmpty(t, resp)
+}
+
+func TestRanobeRoles(t *testing.T) {
+	t.Parallel()
+
+	resp, err := shiki.RanobeRoles(context.Background(), 14893, nil)
+
+	NoError(t, err)
+	NotEmpty(t, resp)
+}
+
+func TestRanobeSimilar(t *testing.T) {
+	t.Parallel()
+
+	resp, err := shiki.RanobeSimilar(context.Background(), 14893, nil)
+
+	NoError(t, err)
+	NotEmpty(t, resp)
+}
+
+func TestRanobeRelated(t *testing.T) {
+	t.Parallel()
+
+	resp, err := shiki.RanobeRelated(context.Background(), 14893, nil)
+
+	NoError(t, err)
+	NotEmpty(t, resp)
+}
+
+func TestRanobeExternalLinks(t *testing.T) {
+	t.Parallel()
+
+	resp, err := shiki.RanobeExternalLinks(context.Background(), 14893, nil)
+
+	NoError(t, err)
+	NotEmpty(t, resp)
+}
+
+func TestRanobeTopics(t *testing.T) {
+	t.Parallel()
+
+	resp, err := shiki.RanobeTopics(context.Background(), 14893, nil)
+
+	NoError(t, err)
+	NotEmpty(t, resp)
+}
